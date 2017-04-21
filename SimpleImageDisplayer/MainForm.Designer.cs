ee6685d8b0074967d884c708a51b8176abe8b5b2
IronySuzumiya <811073137@qq.com>
2017-04-21 17:22:56 +0800

    still some noises there but mostly alright

---
+++ SimpleImageDisplayer/MainForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SimpleImageDisplayer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbPortName = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnOpenData = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Location = new System.Drawing.Point(61, 12);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(79, 25);
+            this.cmbPortName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "端口号";
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.Location = new System.Drawing.Point(146, 12);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenPort.TabIndex = 2;
+            this.btnOpenPort.Text = "打开串口";
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // picImage
+            // 
+            this.picImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picImage.Location = new System.Drawing.Point(15, 43);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(446, 261);
+            this.picImage.TabIndex = 3;
+            this.picImage.TabStop = false;
+            this.picImage.Paint += new System.Windows.Forms.PaintEventHandler(this.picImage_Paint);
+            // 
+            // serialPort
+            // 
+            this.serialPort.BaudRate = 115200;
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(227, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "刷新串口";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnOpenData
+            // 
+            this.btnOpenData.Location = new System.Drawing.Point(308, 12);
+            this.btnOpenData.Name = "btnOpenData";
+            this.btnOpenData.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenData.TabIndex = 5;
+            this.btnOpenData.Text = "打开数据";
+            this.btnOpenData.UseVisualStyleBackColor = true;
+            this.btnOpenData.Click += new System.EventHandler(this.btnOpenData_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 316);
+            this.Controls.Add(this.btnOpenData);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.picImage);
+            this.Controls.Add(this.btnOpenPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbPortName);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "Simple Image Displayer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPortName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnOpenData;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ SimpleImageDisplayer/MainForm.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Ports;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SimpleImageDisplayer
+{
+    public partial class MainForm : Form
+    {
+        private static readonly int IMG_ROW = 50;
+        private static readonly int IMG_COL = 225;
+        private Bitmap image = new Bitmap(IMG_COL, IMG_ROW);
+        private byte[,] imageBuffer = new byte[IMG_ROW, IMG_COL / 8 + 1];
+        private int bufferCol = 0;
+        private int bufferRow = 0;
+
+        private static readonly int ENSURE_CNT_MAX = 5;
+        private static readonly byte FRAME_FLAG = 0xee;
+
+        private int ensureCnt = 0;
+        private bool inFrame = false;
+
+        private bool serialPortIsOpen = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            serialPort.NewLine = "\r\n";
+            btnRefresh.PerformClick();
+            picImage.GetType().GetProperty("DoubleBuffered"
+                , BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(picImage, true, new object[] { });
+            picImage.Image = image;
+        }
+
+        private void picImage_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.ScaleTransform((float)picImage.Width / IMG_COL, (float)picImage.Height / IMG_ROW);
+            e.Graphics.DrawImage(picImage.Image, 0, 0);
+        }
+
+        unsafe private void RenderImage(byte[,] imageBuffer)
+        {
+            var data = image.LockBits(new Rectangle(0, 0, image.Width, image.Height)
+                , ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            for (int row = 0; row < data.Height; ++row)
+            {
+                for (int col = 0; col < data.Width; ++col)
+                {
+                    var color = (byte*)data.Scan0 + row * data.Stride + col * 3;
+                    if ((imageBuffer[row, col / 8] & (0x01 << (col % 8))) != 0)
+                    {
+                        color[0] = 255;
+                        color[1] = 255;
+                        color[2] = 255;
+                    }
+                    else
+                    {
+                        color[0] = 0;
+                        color[1] = 0;
+                        color[2] = 0;
+                    }
+                }
+            }
+            image.UnlockBits(data);
+            picImage.Refresh();
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(100);
+            Invoke(new EventHandler(delegate
+            {
+                var bytes = new byte[serialPort.BytesToRead];
+                serialPort.Read(bytes, 0, bytes.Length);
+                int i = 0;
+                while (i < bytes.Length)
+                {
+                    if (!inFrame)
+                    {
+                        for (; i < bytes.Length; ++i)
+                        {
+                            if (bytes[i] == FRAME_FLAG)
+                            {
+                                ++ensureCnt;
+                            }
+                            else
+                            {
+                                ensureCnt = 0;
+                            }
+                            if (ensureCnt == ENSURE_CNT_MAX)
+                            {
+                                ++i;
+                                ensureCnt = 0;
+                                inFrame = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (inFrame)
+                    {
+                        for (; i < bytes.Length; ++i)
+                        {
+                            if (bufferCol >= IMG_COL)
+                            {
+                                bufferCol = 0;
+                                ++bufferRow;
+                            }
+                            if (bufferRow >= IMG_ROW)
+                            {
+                                bufferRow = 0;
+                                bufferCol = 0;
+                                inFrame = false;
+                                picImage.Refresh();
+                                break;
+                            }
+                            for(int j = 0; j < 8 && bufferCol < IMG_COL; ++bufferCol, ++j)
+                            {
+                                image.SetPixel(bufferCol, bufferRow, (bytes[i] & (0x01 << j)) == 0x00 ? Color.White : Color.Black);
+                            }
+                        }
+                    }
+                }
+            }));
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            foreach (var port in SerialPort.GetPortNames())
+            {
+                cmbPortName.Items.Add(port);
+            }
+            if (cmbPortName.Items.Count != 0)
+            {
+                cmbPortName.SelectedIndex = 0;
+            }
+        }
+
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPortIsOpen)
+                {
+                    serialPort.PortName = cmbPortName.Text;
+                    serialPort.Open();
+                    serialPortIsOpen = true;
+                    btnRefresh.Enabled = false;
+                    btnOpenData.Enabled = false;
+                    btnOpenPort.Text = "关闭串口";
+                }
+                else
+                {
+                    serialPort.Close();
+                    serialPortIsOpen = false;
+                    btnRefresh.Enabled = true;
+                    btnOpenData.Enabled = true;
+                    btnOpenPort.Text = "打开串口";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOpenData_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            using (var file = File.OpenRead(openFileDialog.FileName))
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    var content = reader.ReadToEnd();
+                    var hexStringArray = content.Trim().Split(' ', '\n');
+                    byte value;
+                    for(int i = 0; i < hexStringArray.Length; ++i)
+                    {
+                        value = byte.Parse(hexStringArray[i], System.Globalization.NumberStyles.HexNumber);
+                        if (value != 0x00)
+                        {
+                            imageBuffer[i / IMG_COL, (i % IMG_COL) / 8] |= (byte)(0x01 << (i % IMG_COL % 8));
+                        }
+                        else
+                        {
+                            imageBuffer[i / IMG_COL, (i % IMG_COL) / 8] &= (byte)~(0x01 << (i % IMG_COL % 8));
+                        }
+                    }
+                    RenderImage(imageBuffer);
+                }
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (serialPortIsOpen)
+                {
+                    serialPort.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
